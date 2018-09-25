7107f09112d1b0b41e543a3ba1579ca6ae5f0580
cfgsoft <cfgsoft@yandex.ru>
2018-09-25 16:03:43 +0400

    'order7'

--- resources/views/m/readbarcode/index.blade.php
+++ resources/views/m/readbarcode/index.blade.php
@@ -4,7 +4,7 @@
     <h6><a href="{{ route('m.home.index') }}" >0-Выход</a></h6>
     <p>Отсканируйте штрих код</p>
 
-    <form action="{{ action('m\ReadBarCodeController@submitbarcode') }}" id="formInputBarCode" method="get">
+    <form action="{{ action('m\ReadBarCodeController@submitbarcode') }}" id="formInputBarCode" method="post">
         <input id="InputBarCode" name="BarCode" title="Barcode" size="23" />
         <input type="hidden" name="_token" value="{{ csrf_token() }}" >
         <input type="submit" value=".." />
