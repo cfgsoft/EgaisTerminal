5bf666e2e602602a8e6177b0d21f9a647a7758e0
cfgsoft <cfgsoft@yandex.ru>
2019-05-11 12:58:00 +0400

    'updateExciseStamp'

--- app/Models/ExciseStamp/ExciseStamp.php
+++ app/Models/ExciseStamp/ExciseStamp.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExciseStamp extends Model
 {
-    protected $fillable = ['id', 'productcode','f1regid', 'f2regid'];
+    protected $fillable = ['id', 'productcode','f1regid', 'f2regid', 'department_id', 'barcode'];
 
     public $incrementing = false;
 }
